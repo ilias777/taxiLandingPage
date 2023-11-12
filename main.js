12562a1978eed451622bf32acf30c00400413bb4
Ilias <dougias.ilias@gmail.com>
2023-11-12 10:12:02 +0200

    Add animations to main.js

--- main.js
+++ main.js
@@ -1,2 +1,46 @@
 import "./style.scss";
 import * as bootstrap from "bootstrap";
+
+// Animation One
+const targetOne = document.querySelectorAll(".scrollanimation");
+
+const options = {
+  threshold: 0.3,
+};
+
+const callbackOne = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+      observerOne.unobserve(entry.target);
+    }
+  });
+};
+
+export const observerOne = new IntersectionObserver(callbackOne, {
+  threshold: 0.3,
+});
+
+targetOne.forEach((entry) => {
+  observerOne.observe(entry);
+});
+
+// Animation Two
+const targetTwo = document.querySelectorAll(".scrollIn");
+
+const callbackTwo = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visibleIn");
+      observerTwo.unobserve(entry.target);
+    }
+  });
+};
+
+export const observerTwo = new IntersectionObserver(callbackTwo, {
+  threshold: 0.3,
+});
+
+targetTwo.forEach((entry) => {
+  observerTwo.observe(entry);
+});
